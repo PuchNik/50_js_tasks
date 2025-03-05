9dd5e0dc75474bffea2987371949fd036c6d304b
PuchNik <i.p.chn.k@gmail.com>
2025-03-06 00:07:27 +0300

    Завершение 4 блока заданий

---
+++ part_3/task_4.js
@@ -0,0 +1,9 @@
+const names = ['Bob', 'Klar', 'Petya', 'Ilya', 'Viktoriya'];
+
+function generateItem() {
+    return `<ul>\n` +
+        names.map(name => `    <li> ${name} </li>>`).join('\n')
+    + `\n<ul>`
+}
+
+console.log(generateItem(names))
